94f52e9fb4a0e8308e5ddff85a3b20ad385e53fd
Lucas Kuczek <lucas32kzk@gmail.com>
2023-02-02 11:27:27 +0100

    Initial Commit

---
+++ Camp-2023/Camp_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Camp_2023App.swift
+//  Camp-2023
+//
+//  Created by Lucas Kuczek on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Camp_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
